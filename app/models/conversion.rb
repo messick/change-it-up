fddeb94c3c5471f8c4d1ef91047afd55b96243f6
Nick Messick <nmessick@gmail.com>
2013-05-27 16:52:45 -0600

    clean up model validations

--- app/models/conversion.rb
+++ app/models/conversion.rb
@@ -3,17 +3,14 @@ class Conversion < ActiveRecord::Base
   attr_accessor :width,
     :height, :crop_type
 
-  attr_accessible :conv_type, :height, :image, :width
+  attr_accessible :crop_type, :height, :image, :width
 
-  with_options :presence => true do |inner|
-    inner.validates :crop_type
-    inner.validates :height
-    inner.validates :url
-    inner.validates :width
-  end
+  validates :url, :presence => true
 
   with_options :numericality => { :greater_than => 0 } do |inner|
     inner.validates :height
     inner.validates :width
   end
+
+  validates :crop_type, :inclusion => { :in => %w(fill fit), :message => "value must be either 'fit' or 'fill'" }
 end
