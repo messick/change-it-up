6260688e44b9f66f0181dbd6d6aecdcca9bbb146
Nick Messick <nmessick@gmail.com>
2013-05-27 16:53:09 -0600

    better redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 ChangeItUp::Application.routes.draw do
   match '*url' => 'convert#show'
-  root :to => redirect("/http://change-it-up.herokuapp.com/assets/test.jpg?width=500&height=500&conv_type=resize")
+  root :to => redirect("/http://change-it-up.herokuapp.com/assets/test.jpg?width=500&height=500&crop_type=fit")
 end
