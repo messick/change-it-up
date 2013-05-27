0a00a9b2c6b2c6ce61080f5c01efebefbf736532
Nick Messick <nmessick@gmail.com>
2013-05-27 14:13:38 -0600

    dynamic image manipulation with Carrierwave

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,4 +8,16 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   storage :file
 
+  process :dynamic_conversion
+
+  def dynamic_conversion
+    type = model.conv_type ||= 'resize'
+
+    if type == 'resize'
+      resize_to_fit model.width, model.height
+    else
+      resize_to_fill model.width, model.height
+    end
+  end
+
 end
