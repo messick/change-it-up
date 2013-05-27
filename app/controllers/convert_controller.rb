d521ec117526e4f3365a022e7fedf2f909f324d7
Nick Messick <nmessick@gmail.com>
2013-05-27 15:07:23 -0600

    add some error handling

--- app/controllers/convert_controller.rb
+++ app/controllers/convert_controller.rb
@@ -1,5 +1,3 @@
-require 'net/http'
-
 class ConvertController < ApplicationController
   def show
     uri = "#{params[:url]}.#{params[:format]}"
@@ -8,12 +6,22 @@ class ConvertController < ApplicationController
     # We need to put it back
     re = /(http:|https:)\/([^\/])/
     uri.gsub!(re, "\\1//\\2")
-    image = MiniMagick::Image.open uri
 
-    @conversion = Conversion.new(:width => params[:width].to_i,
-                    :height => params[:height].to_i,
-                    :conv_type => params[:conv_type],
-                    :image => image)
+    begin
+      image = MiniMagick::Image.open uri
+      @conversion = Conversion.new(:width => params[:width].to_i,
+                      :height => params[:height].to_i,
+                      :conv_type => params[:conv_type],
+                      :image => image)
+
+      # Check for validation errors that may have failed the conversion
+      @conversion.valid?
+      flash[:alert] = @conversion.errors.full_messages
+
+    rescue OpenURI::HTTPError
+      # tell the user their image wasn't good enough
+      flash[:alert] = 'Image not found, pleae check URL.'
+    end
 
     render template: 'convert/show',
       handlers: [:haml],
