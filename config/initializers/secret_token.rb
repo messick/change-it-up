e8b32b1daf9bfba6267aad3b1d937241f90d615c
Nick Messick <nmessick@gmail.com>
2013-05-27 11:10:06 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChangeItUp::Application.config.secret_token = '5f3f22f9bc540868394f85f997e35a3167327a45f3a88afdfc6b97a8aabe108c3ffd5bb804d79fc17d823f405e71e68110a9601882385ff3165c3ddf4b238cfa'
