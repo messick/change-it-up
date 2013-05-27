c3a3974421128a54a36ebde93e18b0e806b56b85
Nick Messick <nmessick@gmail.com>
2013-05-27 15:07:38 -0600

    since we have a model, build some validations

--- app/models/conversion.rb
+++ app/models/conversion.rb
@@ -5,4 +5,16 @@ class Conversion < ActiveRecord::Base
 
   attr_accessible :conv_type, :height, :image, :width
   mount_uploader :image, ImageUploader
+
+  with_options :presence => true do |inner|
+    inner.validates :conv_type
+    inner.validates :height
+    inner.validates :image
+    inner.validates :width
+  end
+
+  with_options :numericality => { :greater_than => 0 } do |inner|
+    inner.validates :height
+    inner.validates :width
+  end
 end
